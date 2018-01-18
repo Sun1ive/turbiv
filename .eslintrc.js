2b797f62c229a056d3cf8fd269f543dc60fbe1ad
sun1ive <sunliveua@gmail.com>
2018-01-18 15:32:03 +0200

    hello there

---
+++ .eslintrc.js
@@ -0,0 +1,49 @@
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 2017,
+    sourceType: 'module',
+  },
+  env: {
+    browser: true,
+  },
+  extends: [
+    'airbnb-base',
+    'prettier',
+    'plugin:vue/base',
+    'plugin:vue/essential',
+    'plugin:vue/strongly-recommended',
+    'plugin:vue/recommended',
+  ],
+  // required to lint *.vue files
+  plugins: ['vue', 'import'],
+  // check if imports actually resolve
+  settings: {
+    'import/resolver': {
+      webpack: {
+        config: 'build/webpack.base.conf.js',
+      },
+    },
+  },
+  // add your custom rules here
+  rules: {
+    // don't require .vue extension when importing
+    'import/extensions': 0,
+    'linebreak-style': 0,
+    'no-param-reassign': [2, { props: false }],
+    'no-underscore-dangle': 0,
+    'vue/max-attributes-per-line': 0,
+    // allow optionalDependencies
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        optionalDependencies: ['test/unit/index.js'],
+      },
+    ],
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+  },
+};
---
+++ push.sh
@@ -0,0 +1,3 @@
+git add .
+git commit -m 'hello there'
+git push
