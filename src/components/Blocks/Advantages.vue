948554cc13741b1ffe55ae925668a35946c2397d
Sun1ive <sunliveua@gmail.com>
2018-02-07 15:16:50 +0200

    upd

--- src/components/Blocks/Advantages.vue
+++ src/components/Blocks/Advantages.vue
@@ -3,8 +3,8 @@
     <div class="wrapper">
       <div class="container-fluid">
         <div class="row">
-          <div class="col advantage" v-for="(item, i) in items" :key="i">
-            <img :src="item.src">
+          <div class="col advantage" v-for="(item, i) in advantages" :key="i">
+            <img class="advantage__img" :src="item.src">
             <div class="advantage__text">{{ item.first }}</div>
             <div class="advantage__text">{{ item.second }}</div>
           </div>
@@ -16,35 +16,41 @@
 
 <script>
 export default {
-  data: () => ({
-    items: [
-      { src: '/static/advantages/1.jpg', first: 'Быстрое', second: 'исполнение заказа' },
-      { src: '/static/advantages/2.jpg', first: 'Конкурентные', second: 'цены' },
-      { src: '/static/advantages/3.jpg', first: 'Заказы из', second: '4 стран ЕВРОПЫ' },
-    ]
-  }),
+  computed: {
+    advantages() {
+      if (this.$i18n.locale === 'ru') {
+        return this.$i18n.messages.ru.advantages
+      }
+      return this.$i18n.messages.en.advantages
+    },
+  },
 };
 </script>
 
 
 <style scoped lang="stylus">
 .advantages
-  position absolute
-  top -15%
-  z-index 5
-  left 50%
-  transform translateX(-50%)
-  width 100%
+  position: absolute;
+  top: -15%;
+  z-index: 5;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 100%;
 
 .container-fluid
-  background-color #fff
+  background-color: #fff;
+
+
 .advantage
-  margin 2rem 0
-  text-align center
-  &:nth-child(2)
-  &:nth-child(3)
-    border-left 1px solid #c0c0c0 + 25%
+  margin: 2rem 0;
+  text-align: center;
+  &:nth-child(2), &:nth-child(3)
+    border-left: 1px solid #c0c0c0 + 25%;
   &__text
-    font-size 2rem
-    font-weight bold
+    font-size: 2rem;
+    font-weight: bold
+  &__img
+    height 200px
+    width 300px
+
 </style>
